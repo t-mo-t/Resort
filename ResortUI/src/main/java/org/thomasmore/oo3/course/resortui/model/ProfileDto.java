051c4de221d1d2882c97c8ea335d0c9aba8caf3b
Moegie <Moegie@10.68.245.173>
2014-11-20 11:51:45 +0100

    profilepage test content

---
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/model/ProfileDto.java
@@ -0,0 +1,38 @@
+
+package org.thomasmore.oo3.course.resortui.model;
+
+/**
+ *
+ * @author Moegie
+ */
+public class ProfileDto {
+    String username;
+    String fullname = "TESTJE";
+    String sex;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    
+    
+}
