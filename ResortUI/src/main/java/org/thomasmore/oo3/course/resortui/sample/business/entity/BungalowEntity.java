2a7e927c3fc2ac4c89b69b6d2f9d876c9ed7d929
Arne P. <arath@hotmail.be>
2014-11-27 10:38:56 +0100

    Testing profile

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/sample/business/entity/BungalowEntity.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/sample/business/entity/BungalowEntity.java
@@ -20,6 +20,27 @@ package org.thomasmore.oo3.course.resortui.sample.business.entity;
  *
  * @author Arne
  */
-public class Bungalow {
+public class BungalowEntity {
+    
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
     
 }
