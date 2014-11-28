6866430d92f0dc0f4048b3c2a657df168bcb14f3
Franckx Nico <nicofranckx@hotmail.com>
2014-11-28 09:41:26 +0100

    z
    A first test commiting

---
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/sample/business/entity/ParkEntity.java
@@ -0,0 +1,29 @@
+
+package org.thomasmore.oo3.course.resortui.sample.business.entity;
+
+/**
+ *
+ * @author Jasper & Nico
+ */
+public class ParkEntity {
+    private String id;
+    private String naam;  
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+    
+    
+}
