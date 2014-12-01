d7d5e1a09bdd9deaab5423ae97adbc58b14f7464
Gunther Van Kelst <gunthervankelst@hotmail.com>
2014-12-01 12:01:46 +0100

    park added

---
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/model/ParkDetailDto.java
@@ -0,0 +1,6 @@
+package org.thomasmore.oo3.course.resortui.model;
+
+
+public class ParkDetailDto extends ParkListDetailDto{
+    
+}
---
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/model/ParkPageDto.java
@@ -0,0 +1,28 @@
+package org.thomasmore.oo3.course.resortui.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class ParkPageDto {
+    private ParkDetailDto detail = new ParkDetailDto();
+    private List<ParkListDetailDto> list = new LinkedList<>();
+
+    public ParkDetailDto getDetail() {
+        return detail;
+    }
+
+    public void setDetail(ParkDetailDto detail) {
+        this.detail = detail;
+    }
+
+    public List<ParkListDetailDto> getList() {
+        return list;
+    }
+
+    public void setList(List<ParkListDetailDto> list) {
+        this.list = list;
+    }
+    
+    
+}
