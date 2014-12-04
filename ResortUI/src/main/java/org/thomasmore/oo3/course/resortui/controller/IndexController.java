e9d64d5a750961ca5853c6853efbb5d885aa96f6
marnixvanlint <marnixvanlint@gmail.com>
2014-12-04 10:02:54 +0100

    Merge origin/master into version-teamAwesome
    Conflicts:
    	ResortUI/nb-configuration.xml
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/IndexController.java
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/model/ProfilePageDto.java
    	ResortUI/src/main/webapp/common/template.xhtml
    	ResortUI/src/main/webapp/profile.xhtml
    	ResortUI/src/main/webapp/resources/css/default.css

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/IndexController.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/IndexController.java
@@ -17,15 +17,13 @@
  */
 package org.thomasmore.oo3.course.resortui.controller;
 
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
-import org.thomasmore.oo3.course.resortui.model.IndexPageDto;
+import oo3.course.resortui.model.IndexPageDto;
 import org.thomasmore.oo3.course.resortui.sample.business.entity.SampleParkEntity;
 import org.thomasmore.oo3.course.resortui.sample.dao.SampleParkDao;
 
