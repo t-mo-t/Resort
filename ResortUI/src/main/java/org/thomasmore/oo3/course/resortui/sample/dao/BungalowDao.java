848a03357ff13cdd760d7d739ddc3c42a38a18be
Gunther Van Kelst <gunthervankelst@hotmail.com>
2014-12-14 12:12:01 +0100

    connectie DB naar webpage aangemaakt

---
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/sample/dao/BungalowDao.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thomasmore.oo3.course.resortui.sample.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import org.thomasmore.oo3.course.resortui.sample.business.entity.SampleBungalowEntity;
+import org.thomasmore.oo3.course.resortui.sample.business.entity.SampleParkEntity;
+
+/**
+ *
+ * @author Gun
+ */
+    @Stateless
+    public class BungalowDao implements Serializable {
+
+        @PersistenceContext
+        private EntityManager em;
+
+        public void save(SampleBungalowEntity parkEntity) {
+            em.persist(parkEntity);
+        }
+
+        public List<SampleBungalowEntity> listAll() {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(SampleBungalowEntity.class));
+            Query q = em.createQuery(cq);
+            return q.getResultList();
+        }
+
+        public SampleBungalowEntity findById(String id) {
+            return em.find(SampleBungalowEntity.class, id);
+        }
+
+        public void deleteById(String id) {
+            em.remove(findById(id));
+        }
+    }
+
