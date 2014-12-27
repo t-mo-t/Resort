dd26a1486aee8ecd662cd9d706b3f8d8f85e5255
stephandp <stephandeplucker@hotmail.com>
2014-12-27 15:40:52 +0100

    Merge master into version-DreamTeam
    Conflicts:
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/BungalowController.java
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/dao/BungalowDao.java
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/dao/ParkDao.java
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/entity/BungalowEntity.java
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/model/BungalowListDetailDto.java
    	ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/sample/ui/controller/SampleBungalowController.java
    	ResortUI/src/main/webapp/bungalow.xhtml
    	ResortUI/src/main/webapp/park.xhtml
    	ResortUI/src/main/webapp/profile.xhtml

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/ui/model/ParkListDetailDto.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/ui/model/ParkListDetailDto.java
@@ -1,29 +1,58 @@
 
-package org.thomasmore.oo3.course.resortui.ui.model;
 
+package org.thomasmore.oo3.course.resortui.ui.controller;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import org.thomasmore.oo3.course.resortui.ui.model.BungalowPageDto;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.thomasmore.oo3.course.resortui.ui.model.BungalowListDetailDto;
+import org.thomasmore.oo3.course.resortui.business.entity.BungalowEntity;
+import org.thomasmore.oo3.course.resortui.dao.BungalowDao;
+
+/**
+ *
+ * @author lucs
+ */
+@Named(value = "bungalow")
+
+@RequestScoped
 public class ParkListDetailDto {
-    
-    private String id;
-    
-    private String name;
 
-    public String getId() {
-        return id;
+       private BungalowPageDto dto;
+
+    @EJB
+    private BungalowDao bungalowDao;
+    
+    @PostConstruct
+    public void init() {
+        
+        List<BungalowEntity> bungalows = bungalowDao.listAll();
+        dto = new BungalowPageDto();
+        
+     for (BungalowEntity bungalow : bungalows) {
+            BungalowListDetailDto listDetail = new BungalowListDetailDto();
+            listDetail.setId(bungalow.getId());
+            listDetail.setName(bungalow.getName());
+            dto.getList().add(listDetail);
+        }
+       
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void add(){
+        dto.getDetail().setId("NEW");
+        dto.getList().add(dto.getDetail());
+    }
+    
+    
+
+    public BungalowPageDto getDto() {
+        return dto;
     }
 
-    public String getName() {
-        return name;
+    public void setDto(BungalowPageDto dto) {
+        this.dto = dto;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    
-    
 }
