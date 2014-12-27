ea1ae09deae2d8585e8f9207292af8e868380c26
QDockx <quintendockx@hotmail.com>
2014-12-27 20:52:50 +0100

    Custumer-->CustumerEntity

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/entity/CustomerEntity.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/entity/CustomerEntity.java
@@ -1,19 +1,4 @@
-/*
- * Copyright (C) 2014 Stephan
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 package org.thomasmore.oo3.course.resortui.entity;
 
 import java.sql.Date;
@@ -29,7 +14,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "Customer")
 @XmlRootElement
-public class Customer extends BasicEntity{
+public class CustomerEntity extends BasicEntity{
     
 
    private String name;
@@ -61,9 +46,6 @@ public class Customer extends BasicEntity{
         this.Birthdate = Birthdate;
     }
    
-   
-    
-
 
     public String getName() {
         return name;
