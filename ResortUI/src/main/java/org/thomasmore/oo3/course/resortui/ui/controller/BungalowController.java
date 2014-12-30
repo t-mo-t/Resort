8bdbc17b04217d572140ecd25a6aaf40257eec7c
stephandp <stephandeplucker@hotmail.com>
2014-12-30 20:43:40 +0100

    editeren. Fout bij het laden van bungalows doordat geen id wordt meegegeven

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/ui/controller/BungalowController.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/ui/controller/BungalowController.java
@@ -7,7 +7,9 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import org.thomasmore.oo3.course.resortui.ui.model.BungalowPageDto;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
 import org.thomasmore.oo3.course.resortui.ui.model.BungalowListDetailDto;
 import org.thomasmore.oo3.course.resortui.entity.BungalowEntity;
 import org.thomasmore.oo3.course.resortui.dao.BungalowDao;
@@ -26,21 +28,50 @@ public class BungalowController {
     @EJB
     private BungalowDao bungalowDao;
     
-    @PostConstruct
-    public void init() {
-        
-        List<BungalowEntity> bungalows = bungalowDao.listAll();
+@PostConstruct
+    
+//Je gaat deze parameter uitlezen en in de details van de dto opvullen
+public void init() {
         dto = new BungalowPageDto();
-        
-     for (BungalowEntity bungalow : bungalows) {
+
+        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String editId = req.getParameter("edit");
+
+        BungalowEntity be = bungalowDao.findById(editId);
+        if (be != null) {
+            dto.getDetail().setId(be.getId());
+            dto.getDetail().setName(be.getName());
+        }
+
+        List<BungalowEntity> bungalows = bungalowDao.listAll();
+
+
+        for (BungalowEntity bungalow : bungalows) {
             BungalowListDetailDto listDetail = new BungalowListDetailDto();
             listDetail.setId(bungalow.getId());
             listDetail.setName(bungalow.getName());
             dto.getList().add(listDetail);
         }
-       
+
     }
 
+  public String save() {
+        String id = dto.getDetail().getId();
+        BungalowEntity be = null;
+        if (id != null) {
+            be = bungalowDao.findById(id);
+        }
+        if (be == null) {
+            be = new BungalowEntity();
+        }
+        be.setName(dto.getDetail().getName());
+        bungalowDao.save(be);
+
+        // Forces page refresh
+        return "bungalow.xhtml??faces-redirect=true";
+    }
+    
+    
     public void add(){
         dto.getDetail().setId("NEW");
         dto.getList().add(dto.getDetail());
